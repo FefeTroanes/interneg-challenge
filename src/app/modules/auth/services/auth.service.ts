e2149b5b165487bc97f6fbbca1abed32429f8365
FefeTroanes <ftroanes@gmail.com>
2023-08-22 21:13:04 -0300

    Feat: Auth service
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

---
+++ src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Response } from 'src/app/core/models/response.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  apiUrl: string = 'https://interneg.ddns.net/api-challenge';
+
+  sendCredentials(username: string, password: string): Observable<Response> {
+    let url = `${this.apiUrl}/login`;
+
+    let credentials = {
+      username: username,
+      password: password
+    };
+
+    return this.http.post<Response>(url, credentials);
+  }
+}
