66190431965abdbffeae41e86cdc083e6b425673
FefeTroanes <ftroanes@gmail.com>
2023-08-25 11:23:35 -0300

    Table showing clients data
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- src/app/modules/abm/pages/abm-page/abm-page.component.ts
+++ src/app/modules/abm/pages/abm-page/abm-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { AbmService } from '@modules/abm/services/abm.service';
 import { fromEvent } from 'rxjs';
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {SharedModule} from "@shared/shared.module";
+import { Client } from 'src/app/core/models/client.model';
 
 @Component({
   selector: 'app-abm-page',
   standalone: true,
-  imports: [NgbNavModule, NgIf, SharedModule],
+  imports: [NgbNavModule, NgIf, SharedModule, NgForOf],
   templateUrl: './abm-page.component.html',
   styleUrls: ['./abm-page.component.css']
 })
@@ -43,26 +44,44 @@ export class AbmPageComponent {
   }
 
   keysArray: string[] = ["nombre", "cuit", "email", "domicilio", "telefono"];
-  // clientsArray: 
+  clientsArray: Client[] = []
 
   onClientsClick() {
-    this.abmService.getAllClients$(this.recurso).toPromise()
-    .then( response => {
-      // this.keysArray = Object.keys(response.data[0]);
-      // console.log('estee ', this.keysArray);
-      console.log(response.data[0].nombre);
+    // this.abmService.getAllClients$(this.recurso).toPromise()
+    // .then( response => {
+    //   // this.keysArray = Object.keys(response.data[0]);
+    //   // console.log('estee ', this.keysArray);
+    //   console.log(response.data[0].nombre);
 
-      // for (let i=0; i < response.data.length; i++) {
-      //   console.log('estooo ', response.data[i]);
-      // }
+    //   // for (let i=0; i < response.data.length; i++) {
+    //   //   console.log('estooo ', response.data[i]);
+    //   // }
 
 
-      // console.log(response);
-      // console.log(response.data[0].id);
-      // console.log(response.data[1]);
+    //   // console.log(response);
+    //   // console.log(response.data[0].id);
+    //   // console.log(response.data[1]);
+    // })
+    // .catch( err => {
+    //   console.log(err);
+    // });
+
+    this.abmService.getAllClients$(this.recurso).subscribe(data => {
+      for (let i=0; i < data.data.length; i++) {
+        let client: Client = {
+          id: data.data[i].id,
+          cuit: data.data[i].cuit,
+          nombre: data.data[i].nombre,
+          email: data.data[i].email,
+          telefono: data.data[i].telefono,
+          domicilio: data.data[i].domicilio,
+          created_at: data.data[i].created_at,
+          updated_at: data.data[i].updated_at,
+          cuit_formateado: data.data[i].cuit_formateado,
+        }
+        this.clientsArray.push(client);
+      }
+      console.log('client array ', this.clientsArray);
     })
-    .catch( err => {
-      console.log(err);
-    });
   }
 }
--- src/app/modules/abm/services/abm.service.ts
+++ src/app/modules/abm/services/abm.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbmService {
+
+  data: Observable<any> | undefined;
 
   constructor(private http: HttpClient) { }
 
@@ -21,6 +23,7 @@ export class AbmService {
 
     let token = localStorage.getItem('ATO');
     let headers = new HttpHeaders().set("Authorization", `${token}`);
-    return this.http.get(`${this.URL}/${recurso}`, {headers: headers});
+    this.data = this.http.get(`${this.URL}/${recurso}`, {headers: headers});
+    return this.data;
   }
 }
