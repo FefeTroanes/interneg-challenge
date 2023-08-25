935c989af1eb366d43fe2300339c9d386bc4d2b1
FefeTroanes <ftroanes@gmail.com>
2023-08-25 10:55:40 -0300

    Client interface created
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

---
+++ src/app/core/models/client.model.ts
@@ -0,0 +1,11 @@
+export interface Client {
+  id: number,
+  cuit: string,
+  nombre: string,
+  email: string,
+  telefono: string,
+  domicilio: string,
+  created_at: string,
+  updated_at: string,
+  cuit_formateado: string,
+}
